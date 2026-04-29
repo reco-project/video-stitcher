b4e7c423424ce1463a91323e73985d370c9b62b9
Mohamed Taha GUELZIM <mohamedtahaguelzim@gmail.com>
2026-04-29 13:22:38 +0200

    debug: disable windows_subsystem to test console mode

--- crates/reco-gui/src/main.rs
+++ crates/reco-gui/src/main.rs
@@ -1,4 +1,5 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// windows_subsystem disabled for debugging - shows console on Windows
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 //! Reco GUI - Slint-based panoramic video stitcher.
 //!
